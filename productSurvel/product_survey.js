27ea5582836c4150b370e3abe6f6ba3c55a614cb
Sandip Narayan Maharaj <maharaj_sandip@berkeley.edu>
2025-09-27 11:39:10 -0700

    Update product_survey.js

--- productSurvel/product_survey.js
+++ productSurvel/product_survey.js
@@ -1,6 +1,7 @@
 function submitFeedback() {
     alert('Thank you for your valuable feedback');
     document.getElementById('userInfo').style.display = 'block';
+
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
     const email = document.getElementById('email').value;
@@ -8,6 +9,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experiance = document.getElementById('userExperiance').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -16,6 +18,9 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExp').innerHTML = experiance;
+    document.getElementById('user').innerHTML = username;
+    
 }
 
 const submitButton = document.getElementById('submitBtn');
@@ -26,4 +31,3 @@ document.addEventListener('keydown', function(event) {
       submitFeedback();
     }
 });
-
