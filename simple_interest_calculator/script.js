e34da736f4405b2d05bbc0a9f6c545d5670c9a15
Sandip Narayan Maharaj <maharaj_sandip@berkeley.edu>
2025-12-03 17:32:47 -0800

    Update script.js

--- simple_interest_calculator/script.js
+++ simple_interest_calculator/script.js
@@ -1,1 +1,10 @@
-
+function compute()
+{
+    p = document.getElementById("principal").value;
+    
+}
+        
+function updateRate() {
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
