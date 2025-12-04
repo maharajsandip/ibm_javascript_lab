54644ffd2f42b4b193cbb5e334559482c25d5d6c
Sandip Narayan Maharaj <maharaj_sandip@berkeley.edu>
2025-12-04 12:11:18 -0800

    Update scope_lab.js

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -21,18 +21,26 @@ const globalConst = "I'm a global constant";
 // console.log(blockLet);
 // console.log(blockConst);
 
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-}
+//  ----------------------------------------
+//  step 5
+//  ----------------------------------------
 
-show();
+// function show(){
+//     var functionVar = "I'm a block-scoped var";
+//     let functionLet = "I'm a block-scoped let";
+//     const functionConst = "I'm a block-scoped const";
+// }
+
+// show();
 
 // console.log(functionVar); // Throws ReferenceError
 // console.log(functionLet); // Throws ReferenceError
 // console.log(functionConst); // Throws ReferenceError
 
+
+//  ----------------------------------------
+//  Practice
+//  ----------------------------------------
 
 // block scope variables
 {
