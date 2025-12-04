681d4e4fe48c702779403eaac744a8d869b3b49c
Sandip Narayan Maharaj <maharaj_sandip@berkeley.edu>
2025-12-04 12:06:20 -0800

    Create control_flow.js

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,56 @@
+//  ----------------------------------------
+//  step 2
+//  ----------------------------------------
+
+// let userRole = "admin";
+// let accessLevel;
+
+// if (userRole === "admin") {
+//     accessLevel = "Full access granted";
+// } else if (userRole === "manager") {
+//     accessLevel = "Limited access granted";
+// } else {
+//     accessLevel = "No access granted";
+// }
+
+// console.log("Access Level:", accessLevel);
+
+//  ----------------------------------------
+//  step 3
+//  ----------------------------------------
+
+// let userRole = "admin";
+// let isLoggedIn = true;
+// let userMessage;
+
+// if (isLoggedIn) {
+//     if (userRole === "admin") {
+//         userMessage = "Welcome, Admin!";
+//     } else {
+//         userMessage = "Welcome, User!";
+//     }
+// } else {
+//     userMessage = "Please log in to access the system.";
+// }
+
+// console.log("User Message:", userMessage);
+
+
+//  ----------------------------------------
+//  step 4
+//  ----------------------------------------
+
+
+
+//  ----------------------------------------
+//  step 5
+//  ----------------------------------------
+
+
+//  ----------------------------------------
+//  step 6
+//  ----------------------------------------
+
+//  ----------------------------------------
+//  Practice
+//  ----------------------------------------
